c6b81c1f19e232d8fc84b870d34ed1e59e57918f
WSL <18523559538@163.com>
2019-03-05 16:58:35 +0800

    查询用户openid和nickname的service方法 Signed-off-by: WSL <18523559538@163.com>

--- src/main/java/com/luoran/zzbird/service/ITXcxUserService.java
+++ src/main/java/com/luoran/zzbird/service/ITXcxUserService.java
@@ -1,13 +1,27 @@
 package com.luoran.zzbird.service;
 
+import java.util.List;
+
 import com.luoran.zzbird.core.ext.IBaseService;
 import com.luoran.zzbird.entity.biz.TXcxUser;
 
 /**
- * @author lifetime
+ * @author wsl
  *
  */
 public interface ITXcxUserService extends IBaseService<TXcxUser>{
 	
+	/**
+	 * 
+	 * @Author wsl  
+	 * @Description: TODO 根据openId查询小程序用户
+	 */
+	TXcxUser queryXcxUserByOpenId(String openId);
 	
+	/**
+	 * 
+	 * @Author wsl  
+	 * @Description: TODO 根据用户名查询小程序用户
+	 */
+	List<TXcxUser> queryXcxUserByNickName(String nickName);
 }
--- src/main/java/com/luoran/zzbird/service/impl/TXcxUserService.java
+++ src/main/java/com/luoran/zzbird/service/impl/TXcxUserService.java
@@ -1,5 +1,7 @@
 package com.luoran.zzbird.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +29,16 @@ public class TXcxUserService extends AbstractBaseService<TXcxUser> implements IT
 	@Override
 	public String add(TXcxUser t) {
 		return super.add(t);
+	}
+
+	@Override
+	public TXcxUser queryXcxUserByOpenId(String openId) {
+		return userDao.queryXcxUserByOpenId(openId);
+	}
+
+	@Override
+	public List<TXcxUser> queryXcxUserByNickName(String nickName) {
+		return userDao.queryXcxUserByNickName(nickName);
 	}
 
 
